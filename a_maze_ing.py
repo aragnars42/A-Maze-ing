20f889de511c14707e4967e0f5d6a71fb3f60d3a
Liza Melo Carrion <liza@mbp-de-liza.home>
2026-05-22 21:47:25 +0100

    new oyput, bfs and coode

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,13 +1,30 @@
-import sys
 from parse import parse
 from dfs import MazeGenerator
 from output import write_maze
+from bfs import bfs_short
 
 
 if __name__ == '__main__':
     maze_parse = parse()
-    gen = MazeGenerator(maze_parse["WIDTH"], maze_parse["HEIGHT"], maze_parse.get("SEED"))
+
+    # create random grid with random, walls
+    gen = MazeGenerator(
+        maze_parse["WIDTH"],
+        maze_parse["HEIGHT"],
+        maze_parse.get("SEED"))
+
+    # open up walls from the ENTRY
     gen.generate(maze_parse["ENTRY"][0], maze_parse["ENTRY"][1])
-    write_maze(maze_parse["OUTPUT_FILE"], gen.maz.get_maze(), (maze_parse["ENTRY"][0], maze_parse["ENTRY"][1]), 
-    (maze_parse["EXIT"][0], maze_parse["EXIT"][1]), "")
-     
+
+    # find path to connect the entry to the eit
+    test = bfs_short(
+        gen.maz, (maze_parse["ENTRY"][0], maze_parse["ENTRY"][1]),
+        (maze_parse["EXIT"][0], maze_parse["EXIT"][1]))
+
+    # write the path found with bfs_short
+    write_maze(
+        maze_parse["OUTPUT_FILE"],
+        gen.maz.get_maze(),
+        (maze_parse["ENTRY"][0], maze_parse["ENTRY"][1]),
+        (maze_parse["EXIT"][0], maze_parse["EXIT"][1]),
+        test)
--- output.py
+++ output.py
@@ -27,12 +27,14 @@ def write_maze(filename: str, maze_grid: List[List[int]], entry: Tuple[int, int]
             for row in maze_grid:
                 hex_line = ""
                 for cell in row:
-                    hex_digit = format(cell, 'x')
-                    hex_line = hex_line + hex_digit
+                    hex_line += format(cell, 'x')
                 f.write(hex_line + "\n")
             f.write("\n")
             f.write(str(entry[0]) + "," + str(entry[1]) + "\n")
             f.write(str(exit_pos[0]) + "," + str(exit_pos[1]) + "\n")
-            f.write(path + "\n")
+            if path is None or path == "":
+                f.write("NO PATH\n")
+            else:
+                f.write(path + "\n")
     except IOError as e:
         print("ERROR - Failed to write:", e)
